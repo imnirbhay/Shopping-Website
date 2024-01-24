146dff04409566ca43cf541b7960f943b15c272d
Asmita Pandey <asmitapandey202@gmail.com>
2024-01-24 12:53:07 +0530

    Project Pushed

---
+++ js/product-details copy.js
@@ -0,0 +1,40 @@
+let user = JSON.parse(sessionStorage.user || null)
+
+if(user == null){
+    location.replace('/login');
+}
+
+
+// loader
+let loader = document.querySelector('.loader');
+let noProductImg = document.querySelector('.no-product');
+
+loader.style.display = 'block';
+
+const setupProducts = () => {
+    fetch('/get-products', {
+        method: 'post',
+        headers: new Headers({'Content-Type': 'application/json'}),
+        body: JSON.stringify({email : 'asmitapandey202@gmail.com'})
+    })
+    .then(res => res.json())
+    .then(data => {
+        loader.style.display = 'none';
+        if(data == 'no products'){
+            noProductImg.style.display = 'block';
+        } else{
+            data.forEach(product => createProduct(product));
+        }
+    })
+}
+
+setupProducts();
+const createProduct = (data) => {
+    let productContainer = document.querySelector('.product-container');
+    productContainer.innerHTML += `
+    <div class="product-card">
+        <a href="/products/${data.id}"><img src="${data.image}" class="product-img" alt=""></a>
+        <p class="product-name">${data.tags[0]} →</p>
+    </div>
+    `;
+}
